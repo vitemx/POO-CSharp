30d547032b635b9f5d85b977ff5fd93567227e0a
Brandon Vite <bramsel.vite@gmail.com>
2023-08-31 00:19:50 -0600

    add objects and classes

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Apuntador apuntadorPlatzi = new Apuntador();
+Apuntador apuntadorPlatzi2 = new Apuntador();
+
+apuntadorPlatzi.Color = "Verde";
+
+apuntadorPlatzi2.Color = "Azul";
+class Apuntador
+{
+    public string? Color;
+    public double Largo;
+    public short NumeroDeBotones;
+    public bool TieneStickers;
+}
